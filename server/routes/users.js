2ff674245bde5343f9995d50b9cfbccaacd37a6b
mcondit <mcondit@hubspot.com>
2020-01-24 22:15:52 -0500

    add dependencies

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,18 +1,33 @@
 const express = require('express');
+
 const router = express.Router();
 
 const Users = require('../db/index').users;
 
 // get all users
 router.get('/', async (req, res, next) => {
-	try {
-		const users = await Users.findAll();
-		res.send(users);
-	} catch(e) {
-		res.status(500).send({
-			message: 'Internal server error'
-		})
-	}
+  try {
+    const users = await Users.findAll();
+    res.send(users);
+  } catch (e) {
+    res.status(500).send({
+      message: 'Internal server error'
+    })
+  }
+});
+
+
+router.get('/:userId', async (req, res, next) => {
+  return res.send({
+    message: 'implement'
+  })
+});
+
+// posting to users creates a new user
+router.post('/', async (req, res, next) => {
+return res.send({
+	message: 'implement'
+});
 });
 
 
